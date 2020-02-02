daa7ce2d30226c2388cd9a0b5f0491d5f7eb063e
Sarthak <37593543+sarthak-expedia@users.noreply.github.com>
2020-02-02 19:20:48 +0530

    Update Topic Classification.py

--- Topic Classification.py
+++ Topic Classification.py
@@ -1,8 +1,14 @@
+
+from textblob import TextBlob
+from textblob.classifiers import NaiveBayesClassifier
 import pandas as pd
-import numpy as np
-from nltk.tokenize import word_tokenize
-from nltk import pos_tag
+import nltk
+nltk.download('brown')
+from textblob import Word
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import numpy as np
+from nltk import pos_tag
 from nltk.stem import WordNetLemmatizer
 from sklearn.preprocessing import LabelEncoder
 from collections import defaultdict
@@ -10,86 +16,140 @@ from nltk.corpus import wordnet as wn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import model_selection, naive_bayes, svm
 from sklearn.metrics import accuracy_score
+import csv
 
-#Set Random seed
+
+#setting random seed
 np.random.seed(500)
 
-# Add the Data using pandas
-Corpus = pd.read_csv(r"C:\Users\gunjit.bedi\Desktop\NLP Project\corpus_small.csv",encoding='latin-1')
-
-# Step - 1: Data Pre-processing - This will help in getting better results through the classification algorithms
-
-# Step - 1a : Remove blank rows if any.
-Corpus['text'].dropna(inplace=True)
-
-# Step - 1b : Change all the text to lower case. This is required as python interprets 'dog' and 'DOG' differently
-Corpus['text'] = [entry.lower() for entry in Corpus['text']]
-
-# Step - 1c : Tokenization : In this each entry in the corpus will be broken into set of words
-Corpus['text']= [word_tokenize(entry) for entry in Corpus['text']]
-
-# Step - 1d : Remove Stop words, Non-Numeric and perfom Word Stemming/Lemmenting.
-
-# WordNetLemmatizer requires Pos tags to understand if the word is noun or verb or adjective etc. By default it is set to Noun
-tag_map = defaultdict(lambda : wn.NOUN)
-tag_map['J'] = wn.ADJ
-tag_map['V'] = wn.VERB
-tag_map['R'] = wn.ADV
+#cleaning training data
+input_0=pd.read_csv(r'\\Mac\Home\Desktop\parallel data\data_6.csv')
+stop_words = set(stopwords.words('english'))
+limit_0=len(input_0.index)
+for i in range(0,limit_0):
+    a = TextBlob(input_0.iloc[i,0])
+    #a = a.correct()
+    a = a.lower()
+    w = Word(a)
+    a = w.lemmatize()
+    input_0.iloc[i,0] = str(a)
+    word_tokens = word_tokenize(input_0.iloc[i,0])
+    #input_0.iloc[i,0] = input_0.iloc[i,0].lemmatize()
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    input_0.iloc[i,0] = " ".join(filtered_sentence)
 
 
-for index,entry in enumerate(Corpus['text']):
-    # Declaring Empty List to store the words that follow the rules for this step
-    Final_words = []
-    # Initializing WordNetLemmatizer()
-    word_Lemmatized = WordNetLemmatizer()
-    # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
-    for word, tag in pos_tag(entry):
-        # Below condition is to check for Stop words and consider only alphabets
-        if word not in stopwords.words('english') and word.isalpha():
-            word_Final = word_Lemmatized.lemmatize(word,tag_map[tag[0]])
-            Final_words.append(word_Final)
-    # The final processed set of words for each iteration will be stored in 'text_final'
-    Corpus.loc[index,'text_final'] = str(Final_words)
+print('file tokenized')
+input_0.to_csv(r'\\Mac\Home\Desktop\input_0.csv')
+print('file printed')
 
-#print(Corpus['text_final'].head())
+#assigning training data
 
-# Step - 2: Split the model into Train and Test Data set
-Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(Corpus['text_final'],Corpus['label'],test_size=0.3)
+Train_X = input_0.iloc[:,0]
+Train_Y = input_0.iloc[:,1]
 
-# Step - 3: Label encode the target variable  - This is done to transform Categorical data of string type in the data set into numerical values
+#encoding
 Encoder = LabelEncoder()
 Train_Y = Encoder.fit_transform(Train_Y)
-Test_Y = Encoder.fit_transform(Test_Y)
+print(Train_Y)
 
-# Step - 4: Vectorize the words by using TF-IDF Vectorizer - This is done to find how important a word in document is in comaprison to the corpus
-Tfidf_vect = TfidfVectorizer(max_features=5000)
-Tfidf_vect.fit(Corpus['text_final'])
+#vectorization
+vectorizer = TfidfVectorizer(max_features=5000)
+vectors = vectorizer.fit_transform(Train_X)
+feature_names = vectorizer.get_feature_names()
+dense = vectors.todense()
 
-Train_X_Tfidf = Tfidf_vect.transform(Train_X)
-Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+#denselist = dense.tolist()
 
-
-# Step - 5: Now we can run different algorithms to classify out data check for accuracy
-
-# Classifier - Algorithm - Naive Bayes
-# fit the training dataset on the classifier
-Naive = naive_bayes.MultinomialNB()
-Naive.fit(Train_X_Tfidf,Train_Y)
-
-# predict the labels on validation dataset
-predictions_NB = Naive.predict(Test_X_Tfidf)
-
-# Use accuracy_score function to get the accuracy
-print("Naive Bayes Accuracy Score -> ",accuracy_score(predictions_NB, Test_Y)*100)
-
+df = pd.DataFrame(dense, columns=feature_names)
+print(df)
 
 # Classifier - Algorithm - SVM
 # fit the training dataset on the classifier
 SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
-SVM.fit(Train_X_Tfidf,Train_Y)
+SVM.fit(vectors,Train_Y)
+print('model trained')
+
+# importing testing data and cleaning it
+input=pd.read_excel(r'\\Mac\Home\Desktop\daily.xls')
+Output=pd.DataFrame(columns=['Comment','Date','Time','Browser','Email','GUID','URL','Category','Top Customer','Exp User ID','Gross Profit','Repeat Purchase Probability','Centile','NID','IP','Language'])
+#print(input.iloc[0,0])
+limit=len(input.index)
+
+for i in range(0,limit):
+    a = TextBlob(input.iloc[i,0])
+    #a = a.correct()
+    a = a.lower()
+    w = Word(a)
+    a = w.lemmatize()
+    input.iloc[i,0] = str(a)
+    word_tokens = word_tokenize(input.iloc[i,0])
+    #input_0.iloc[i,0] = input_0.iloc[i,0].lemmatize()
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    input.iloc[i,0] = " ".join(filtered_sentence)
+
+
+print('testing file tokenized')
+
+Test_X = input.iloc[:,0]
+
+#vectorization
+vectorizer_2 = TfidfVectorizer(max_features=5000)
+vectors_2 = vectorizer_2.fit_transform(Test_X)
 
 # predict the labels on validation dataset
-predictions_SVM = SVM.predict(Test_X_Tfidf)
+predictions_SVM = SVM.predict(vectors_2)
+print(predictions_SVM)
 
 # Use accuracy_score function to get the accuracy
-print("SVM Accuracy Score -> ",accuracy_score(predictions_SVM, Test_Y)*100)
+#print("SVM Accuracy Score -> ",accuracy_score(predictions_SVM, Test_Y)*100)
+
+for i in range(0,limit):
+    Output.at[i,'Comment']=input.iloc[i,0]
+    #cat=cl.classify(input.iloc[i,0])
+    blob1 = TextBlob(input.iloc[i,0])
+    if(float(format(blob1.sentiment.polarity))>0.3):
+        predictions_SVM[i] = '1'
+    else:
+        if ((input.iloc[i,11]=='1.0') or (input.iloc[i,11]=='2.0') or (input.iloc[i,11]=='3.0') or (input.iloc[i,11]=='4.0') or (input.iloc[i,11]=='5.0') or (input.iloc[i,11]=='6.0') or (input.iloc[i,11]=='7.0') or (input.iloc[i,11]=='8.0') or (input.iloc[i,11]=='9.0') or (input.iloc[i,11]=='10.0')) :
+            predictions_SVM[i]='0'
+        elif ((input.iloc[i,7]=='GOLD') or (input.iloc[i,7]=='Gold') or (input.iloc[i,7]=='SILVER') or  (input.iloc[i,7]=='Silver') or (input.iloc[i,7]=='PLATINUM')):
+            predictions_SVM[i]='0'
+        elif ((input.iloc[i,14]!='English') or (input.iloc[i,14]!='en')):
+            predictions_SVM[i]='0'
+        else :
+            print(i)
+    
+    
+    if(predictions_SVM[i]==0):
+        cat='Actionable'
+    else:
+        if(predictions_SVM[i]==1):
+            cat='Non Actionable'
+    Output.at[i,'Category']=cat
+    Output.at[i,'Date']=input.iloc[i,1]
+    Output.at[i,'Time']=input.iloc[i,2]
+    Output.at[i,'Browser']=input.iloc[i,3]
+    Output.at[i,'Email']=input.iloc[i,4]
+    Output.at[i,'GUID']=input.iloc[i,5]
+    Output.at[i,'URL']=input.iloc[i,6]
+    Output.at[i,'Top Customer']=input.iloc[i,7]
+    Output.at[i,'Exp User ID']=input.iloc[i,8]
+    Output.at[i,'Gross Profit']=input.iloc[i,9]
+    Output.at[i,'Repeat Purchase Probability']=input.iloc[i,10]
+    Output.at[i,'Centile']=input.iloc[i,11]
+    Output.at[i,'NID']=input.iloc[i,12]
+    Output.at[i,'IP']=input.iloc[i,13]
+    Output.at[i,'Language']=input.iloc[i,14]
+ 
+#b=cl.accuracy(test)
+Output.to_excel(r'\\Mac\Home\Desktop\output_NLP.xlsx')
+print('output written')
+
+
+
+
+
+
+
+
